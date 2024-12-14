afd2164d677f219e2eba97923353477f55bb716c
MsTrille <trishiagavile0911@gmail.com>
2024-12-14 13:42:04 +0800

    Handling messages

--- main.go
+++ main.go
@@ -16,4 +16,18 @@ func main() {
 	if err != nil {
 		log.fatal("error creating discord session: %v", err)
 	}
+	log.Println("Bot is running. Press CTRL+C to exit.")
+	select {}
+}
+
+// handle incoming messages
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore messages from bot itself
+	IF m.Author.ID == s.State.User.ID {
+		return
+	}
+	// reponse to messages
+	if m.Content == !chat {
+		s.ChannelMessageSend(m.ChannelID, "Hello, How can I help you?")
+	}
 }
