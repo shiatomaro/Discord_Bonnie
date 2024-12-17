f055d794f6b4e1331781e4f795b5812cfa6bb052
MsTrille <trishiagavile0911@gmail.com>
2024-12-17 11:54:11 +0800

    token

--- main.go
+++ main.go
@@ -10,54 +10,61 @@ import (
 )
 
 func main() {
-	// get bot token from the environment
+	// Get bot token from the environment
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	if token == "" || apiKey == "" {
-		log.Fatal("No Token Provided or Required Variables missing. set DISCORD_BOT_TOKEN in environment or OPENAUI_API_KEY")
+		log.Fatal("No Token Provided or Required Variables missing. set DISCORD_BOT_TOKEN in environment or OPENAI_API_KEY")
 	}
 
-	//creating new Openai client
-	openAIClient := openai.NewClient(apiKey)
-
 	// Creating new session
-	bot, err := discordgo.New("Bot" + token)
+	bot, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalf("error creating discord session: %v", err)
 	}
 
-	bot.AddMessageCreateEventHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		// ignore messages from bot itself
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-		// reponse to messages using "!chat"
-		if len(m.Content) > 5 && m.Content[:5] == "!chat" {
-			//get  message after "!chat"
-			userMessage := m.Content[6:]
+	// Register message handler
+	bot.AddHandler(messageCreateHandler)
 
-			//Query openAi
-			response, err := queryChatGPT(openAIClient, userMessage)
-			if err != nil {
-				log.Printf("Error quering OpenAi: %v", err)
-				s.ChannelMessageSend(m.ChannelID, "Hello, Sorry I couldn't process your request")
-				return
-			}
-			// Send the response to Discord
-			s.ChannelMessageSend(m.ChannelID, response)
-		}
-	})
-	// opeing connection to discord
+	// Opening connection to discord
 	err = bot.Open()
 	if err != nil {
 		log.Fatalf("Error opening connection to discord: %v", err)
 	}
 	log.Println("Bot is running. Press CTRL+C to exit!")
-	select {} // to keep ruuning the bot
+	select {} // to keep running the bot
+}
+
+// Handle message create event
+func messageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages from the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Respond to messages using "!chat"
+	if len(m.Content) > 5 && m.Content[:5] == "!chat" {
+		// Get message after "!chat"
+		userMessage := m.Content[6:]
+
+		// Query OpenAI
+		response, err := queryChatGPT(userMessage)
+		if err != nil {
+			log.Printf("Error querying OpenAI: %v", err)
+			s.ChannelMessageSend(m.ChannelID, "Hello, Sorry I couldn't process your request")
+			return
+		}
+
+		// Send the response to Discord
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
 }
 
 // Query OpenAI API with the user's message
-func queryChatGPT(client *openai.Client, message string) (string, error) {
+func queryChatGPT(message string) (string, error) {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	client := openai.NewClient(apiKey)
+
 	req := openai.ChatCompletionRequest{
 		Model: openai.GPT3Dot5Turbo,
 		Messages: []openai.ChatCompletionMessage{
